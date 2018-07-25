88155c0dcf7c50cc4a0c39d586e4eb19c9e88537
Andre Cavalcante <andreluiz_pinho@hotmail.com>
2018-07-25 14:18:22 -0300

    add validates to Restaurant model and seeds the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Amarelinho',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '555-8978',
+    category:     'chinese'
+  },
+  {
+    name:         'Paladar da Vila',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '589-7978',
+    category:     'italian'
+  },
+  {
+    name: 'Arigato',
+    address: 'Rua da Liberdade 800',
+    phone_number: '897-5678',
+    category: 'japanese'
+  },
+  {
+    name: '2x1',
+    address: 'Copa da Russia 2018',
+    phone_number: '212-1212',
+    category: 'belgian'
+  },
+  {
+    name: 'The French House',
+    address: 'Rua Mourato 134',
+    phone_number: '734-9087',
+    category: 'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
